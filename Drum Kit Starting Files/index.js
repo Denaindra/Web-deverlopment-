32ca64f4719fda8f589f83dc14cd45124ba85de6
mgdperera <mgdperera@gmail.com>
2019-10-15 20:51:30 +0530

    add commit files

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,7 @@
+var buttons = document.querySelectorAll("button");
+  
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+      alert("hellow");
+    });
+}
